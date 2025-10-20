530f7abb52480ae6c6f310700b6523e99f18443b
Sheikh Ubaid Ullah <ubaidullah.gr8@gmail.com>
2025-10-20 07:29:46 +0000

    sdasd

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -1,10 +1,13 @@
-import { HandHeart } from 'lucide-react';
 import Link from 'next/link';
 
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2" aria-label="Sawaid Al islah Homepage">
-      <HandHeart className="h-8 w-8 text-accent" />
+      <img
+        src="https://img.notionusercontent.com/s3/prod-files-secure%2F95988344-badb-4daa-8013-19c466f371f7%2F841403be-f6ba-4c88-935d-9f3b34776d06%2FWhatsApp_Image_2025-10-15_at_11.41.36_AM.jpeg/size/w=2000?exp=1760945605&sig=FHz007HSWiY30OtYpT5DNdKGo8tWaqpNsdzf0Gj-yeg&id=2914732f-17cc-8066-a318-e6fc20abb82b&table=block&userId=90cb780f-86ce-4aeb-881a-664101056505"
+        alt="Sawaid Al islah Logo"
+        className="h-8 w-8 object-contain"
+      />
       <span className="text-xl font-headline font-bold text-primary-foreground">
         Sawaid Al islah
       </span>
