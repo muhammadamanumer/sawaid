470de3046a45c258088e9be0d83abcedcf5a7f16
muhammadamanumer <khanaman9325@gmail.com>
2025-10-24 02:15:15 +0500

    style: standardize formatting and improve consistency in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,22 @@
-import type { Metadata } from 'next';
-import './globals.css';
+import type { Metadata } from "next";
+import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-import { TranslationWrapper } from '@/hooks/use-translation';
-import { Header } from '@/components/layout/header';
-import { Footer } from '@/components/layout/footer';
+import { TranslationWrapper } from "@/hooks/use-translation";
+import { Header } from "@/components/layout/header";
+import { Footer } from "@/components/layout/footer";
+//pushed from local repo
 
 export const metadata: Metadata = {
-  title: 'Sawaid Al Islah',
-  description: 'Providing relief and hope to communities in need.',
+  title: "Sawaid Al Islah",
+  description: "Providing relief and hope to communities in need.",
   icons: {
-    icon: [
-      { url: '/Logo/sawaid-logo-radius.webp', type: 'image/webp' },
-    ],
+    icon: [{ url: "/Logo/sawaid-logo-radius.webp", type: "image/webp" }],
     apple: [
-      { url: '/Logo/sawaid-logo-radius.webp', sizes: '180x180', type: 'image/webp' },
+      {
+        url: "/Logo/sawaid-logo-radius.webp",
+        sizes: "180x180",
+        type: "image/webp",
+      },
     ],
   },
 };
@@ -28,15 +31,22 @@ export default function RootLayout({
     <html>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Cairo:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Cairo:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body className="font-body antialiased">
         <TranslationWrapper>
-            <Header />
-            {children}
-            <Footer />
-            <Toaster />
+          <Header />
+          {children}
+          <Footer />
+          <Toaster />
         </TranslationWrapper>
       </body>
     </html>
